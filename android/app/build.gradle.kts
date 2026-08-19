769aff2c907adc6b569b62ee0502ad0d334c168d
MOHAMED ASIK.K <edumohamedasik@gmail.com>
2026-08-19 23:53:05 +0530

    feat: integrate Firebase and Google Services into Android build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,6 +49,7 @@ flutter {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
 }
 
 configurations.all {
